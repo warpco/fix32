b872fc716fc3f68452af7ed34317eef93889268a
Dmitry Tsvyk <tsvykd@gmail.com>
2016-09-25 00:21:39 +0300

    Add a prototype for the division function

--- inc/fix32.h
+++ inc/fix32.h
@@ -25,4 +25,31 @@
 fix32_t fix32_inv(fix32_t a, fix32_t exp);
 
 
+//
+// Returns the quotient of two fixed-point numbers.
+//
+// @param a
+// Fixed-point value which represents a dividend. This value can have any
+// format, from Q1 to Q31. The third argument specifies the actual format.
+//
+// @param b
+// Non-zero fixed-point value representing a divisor. This value can have
+// any fixed-point format, from Q1 to Q31. The actual format is specified
+// in the third argument. If the value is zero, the behavior is undefined.
+//
+// @param exp
+// Value specifying the format of both the dividend and divisor. The value
+// must be in the range from 1 to 31, which corresponds to the fixed-point
+// formats from Q1 to Q31 respectively. If the value exceeds these limits,
+// the behavior is undefined.
+//
+// @return
+// The quotient of two specified fixed-point numbers. The result will have
+// the same fixed-point format as the input values. If the quotient cannot
+// be represented in this format, or the first two arguments are specified
+// in different formats, the behavior is undefined.
+//
+fix32_t fix32_div(fix32_t a, fix32_t b, fix32_t exp);
+
+
 #endif
