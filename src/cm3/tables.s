9ac29e87a5297a417189f2cf314304ed96de8360
Dmitry Tsvyk <tsvykd@gmail.com>
2016-09-26 14:34:04 +0400

    Add the sine lookup table

--- src/cm3/tables.s
+++ src/cm3/tables.s
@@ -1,6 +1,7 @@
 
                     export  fix32_inv_table
                     export  fix32_isqrt_table
+                    export  fix32_sin_table
 
 
 ; The reciprocal lookup table. The table consist of 256 eight-bit entries.
@@ -51,6 +52,30 @@ fix32_isqrt_table   dcd     0x05a005a5, 0x0595059a, 0x058a058f, 0x05800585
                     dcd     0x04300433, 0x042c042e, 0x04270429, 0x04230425
                     dcd     0x041e0420, 0x041a041c, 0x04160418, 0x04110414
                     dcd     0x040d040f, 0x0409040b, 0x04050407, 0x04010403
+
+
+; The sine lookup table. The table consist of sixty-four 32-bit entries. Each
+; table entry is a Q31 fixed-point value representing the sine of an angle in
+; the first quadrant.
+
+                    area    |.fix32_sin_table|, data
+
+fix32_sin_table     dcd     0x01921d20, 0x04b6195d, 0x07d95b9e, 0x0afb6805
+                    dcd     0x0e1bc2e4, 0x1139f0cf, 0x145576b1, 0x176dd9de
+                    dcd     0x1a82a026, 0x1d934fe5, 0x209f701c, 0x23a6887f
+                    dcd     0x26a82186, 0x29a3c485, 0x2c98fbba, 0x2f875262
+                    dcd     0x326e54c7, 0x354d9057, 0x382493b0, 0x3af2eeb7
+                    dcd     0x3db832a6, 0x4073f21d, 0x4325c136, 0x45cd3590
+                    dcd     0x4869e665, 0x4afb6c98, 0x4d8162c4, 0x4ffb654d
+                    dcd     0x5269126f, 0x54ca0a4b, 0x571deefa, 0x59646498
+                    dcd     0x5b9d1154, 0x5dc79d7c, 0x5fe3b38e, 0x61f1003f
+                    dcd     0x63ef3290, 0x65ddfbd3, 0x67bd0fbd, 0x698c246c
+                    dcd     0x6b4af279, 0x6cf934fc, 0x6e96a99d, 0x7023109a
+                    dcd     0x719e2cd3, 0x7307c3d0, 0x745f9dd1, 0x75a585d0
+                    dcd     0x76d94989, 0x77fab989, 0x7909a92d, 0x7a05eeae
+                    dcd     0x7aef6324, 0x7bc5e290, 0x7c894bde, 0x7d3980ed
+                    dcd     0x7dd6668f, 0x7e5fe494, 0x7ed5e5c7, 0x7f3857f6
+                    dcd     0x7f872bf3, 0x7fc25597, 0x7fe9cbc1, 0x7ffd885b
 
 
                     end
