86bec5dda610aebf0c7d4db4cf70ccd754df6f31
Dmitry Tsvyk <tsvykd@gmail.com>
2016-09-30 18:23:37 +0300

    Implement the integer-part function

---
+++ src/cm3/gcc/trun.s
@@ -0,0 +1,34 @@
+
+        .syntax     unified
+        .arch       armv7-m
+        .cpu        cortex-m3
+        .thumb
+
+        .global     fix32_trun
+        .type       fix32_trun, "function"
+
+
+@ Returns the integer part of a fixed-point number specified in the first
+@ argument. The second argument specifies the format of the first argument.
+@ For more information, see the function description in the header file.
+@
+@ Execution time: 9-11 cycles
+@ Absolute error: 0 LSB
+
+        .section   .fix32_trun, "x"
+        .align
+
+@ The algorithm consist of the three steps. First, the input is splitted
+@ to the magnitude and sign part. Next, the fractional bits are discarded.
+@ And the last step restores the sign of the input value.
+
+fix32_trun:
+        tst         r0, r0
+        negmi       r0, r0
+        lsr         r0, r0, r1
+        lsl         r0, r0, r1
+        negmi       r0, r0
+        bx          lr
+
+
+        .end
